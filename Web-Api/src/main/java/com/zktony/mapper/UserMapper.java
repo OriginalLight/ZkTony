b73901851ce88481a3150ead5572d5abf478bb46
(^^) <15252114322@163.com>
2022-10-29 14:24:56 +0800

    Feature 10.29

---
+++ Web-Api/src/main/java/com/zktony/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.zktony.mapper;
+
+import com.zktony.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author liuhh
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2022-10-29 13:27:11
+* @Entity com.zktony.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ Web-Api/src/main/java/com/zktony/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.zktony.service;
+
+import com.zktony.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author liuhh
+* @description 针对表【user】的数据库操作Service
+* @createDate 2022-10-29 13:27:11
+*/
+public interface IUserService extends IService<User> {
+
+}
