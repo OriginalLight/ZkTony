04613b2f2444be6f1863e14abcd4f19607f5f7b0
(^^) <15252114322@163.com>
2022-10-30 13:58:07 +0800

    Feature login

--- Web-Api/src/main/java/com/zktony/service/impl/UserServiceImpl.java
+++ Web-Api/src/main/java/com/zktony/service/impl/UserServiceImpl.java
@@ -1,20 +1,26 @@
 package com.zktony.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zktony.entity.User;
-import com.zktony.service.IUserService;
 import com.zktony.mapper.UserMapper;
+import com.zktony.service.IUserService;
 import org.springframework.stereotype.Service;
 
 /**
-* @author liuhh
-* @description 针对表【user】的数据库操作Service实现
-* @createDate 2022-10-29 13:27:11
-*/
+ * @author liuhh
+ * @description 针对表【user】的数据库操作Service实现
+ * @createDate 2022-10-29 13:27:11
+ */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements IUserService {
+        implements IUserService {
 
+    @Override
+    public User login(User user) {
+        // 返回email和password相同的用户
+        return this.getOne(new QueryWrapper<User>().eq("email", user.getEmail()).eq("password", user.getPassword()));
+    }
 }
 
 
