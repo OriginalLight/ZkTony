116cbc12948ae6fb162dbe41d2748667ad6f6a78
(^^) <15252114322@163.com>
2022-11-22 17:29:24 +0800

    Fix 11.22

--- ZkTony-FY/app/src/test/java/com/zktony/www/ExampleUnitTest.kt
+++ ZkTony-FY/app/src/test/java/com/zktony/www/ExampleUnitTest.kt
@@ -15,16 +15,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    @Test
-    fun hexToAscii() {
-        val str = "43 4D 44 3A 52 45 50 4C 59 3D 32 40 30 0D"
-        assertEquals("CMD:REPLY=2@0\r", str.hexToAscii())
-    }
-
-    @Test
-    fun extractNumber() {
-        val str = "TC1:TCACTUALTEMP=25.6@2\r"
-        assertEquals("25.6", str.extractTemp())
-    }
 }
