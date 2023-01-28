3c952e0282cb982a4656e8d0f8f1f3c283963c32
(^^) <15252114322@163.com>
2023-01-28 17:16:09 +0800

    Feature

--- ZkTony-LIQUID/app/src/main/java/com/zktony/www/ui/container/PlateOneViewModel.kt
+++ ZkTony-LIQUID/app/src/main/java/com/zktony/www/ui/container/PlateOneViewModel.kt
@@ -5,5 +5,5 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ElutionViewModel @Inject constructor() : BaseViewModel() {
+class PlateOneViewModel @Inject constructor() : BaseViewModel() {
 }
