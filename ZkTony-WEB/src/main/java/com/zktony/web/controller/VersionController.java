e03cc45f6f8c06c3777d70e3c81fd64405b4d5f3
(^^) <15252114322@163.com>
2023-02-04 10:01:52 +0800

    Update

--- ZkTony-WEB/src/main/java/com/zktony/web/controller/VersionController.java
+++ ZkTony-WEB/src/main/java/com/zktony/web/controller/VersionController.java
@@ -4,8 +4,6 @@ package com.zktony.web.controller;
 import com.zktony.web.commom.model.Result;
 import com.zktony.web.entity.Version;
 import com.zktony.web.repository.VersionRepository;
-import jakarta.annotation.Resource;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
