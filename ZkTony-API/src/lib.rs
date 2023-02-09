9c12beb2fab473b959c3b09cb55e07d1b1289acd
(^^) <15252114322@163.com>
2023-02-10 00:17:37 +0800

    Fix

--- ZkTony-API/src/lib.rs
+++ ZkTony-API/src/lib.rs
@@ -1,14 +1,6 @@
 pub mod config;
 pub mod entity;
-mod err;
-pub mod form;
 pub mod handler;
-pub mod param;
 pub mod res;
 pub mod router;
 pub mod state;
-pub mod view;
-
-pub use err::{AppError, AppErrorType};
-
-pub type Result<T> = std::result::Result<T, crate::AppError>;
