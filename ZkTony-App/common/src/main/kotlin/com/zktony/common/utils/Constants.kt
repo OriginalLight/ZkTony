08fa4468244cd606f377554d1ebc398f291c55aa
(^^) <15252114322@163.com>
2023-03-23 16:47:19 +0800

    Revert "Fix the voltage"
    This reverts commit 7eced1620791ddfc9083c8bf766e42d7dce1d2d2.

--- ZkTony-App/common/src/main/kotlin/com/zktony/common/utils/Constants.kt
+++ ZkTony-App/common/src/main/kotlin/com/zktony/common/utils/Constants.kt
@@ -18,7 +18,7 @@ object Constants {
     const val NEEDLE_SPACE = "NEEDLE_SPACE"
     const val MAX_MOTOR = 250
     const val MAX_TIME = 99f
-    const val MAX_VOLTAGE_ZM = 65f
+    const val MAX_VOLTAGE_ZM = 50f
     const val MAX_VOLTAGE_RS = 30f
     const val ERROR_CURRENT = 0.1f
     const val ZERO_TIME = "00:00:00"
