9d9103a206e7c827aa568ed37924a21cca7df4bb
(^^) <15252114322@163.com>
2023-04-18 13:54:53 +0800

    Fix

--- ZkTony-App/ZkTony_LIQUID_ONE/build.gradle.kts
+++ ZkTony-App/ZkTony_LIQUID_ONE/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zktony.www"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
