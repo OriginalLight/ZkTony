acd7b5be49d0210d5f1a0fb34ae201eeb8bdcd8c
(^^) <15252114322@163.com>
2023-05-19 23:53:53 +0800

    crc16

--- Android/App_New/serialport/src/main/java/com/zktony/serialport/ext/CrcExt.kt
+++ Android/App_New/serialport/src/main/java/com/zktony/serialport/ext/CrcExt.kt
@@ -1,9 +1,76 @@
 package com.zktony.serialport.ext
 
-const val BITS_OF_BYTE = 8
-const val POLYNOMIAL = 0xA001
-const val INITIAL_VALUE = 0xFFFF
-const val FF = 0xFF
+
+/**
+ * CRC16_CCITT：多项式x16+x12+x5+1（0x1021），初始值0x0000，低位在前，高位在后，结果与0x0000异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16ccitt(): ByteArray {
+    var crc = 0x0000
+    for (j in this) {
+        crc = crc xor (j.toInt() and 0xff)
+        for (i in 0..7) {
+            crc = if (crc and 1 != 0) crc shr 1 xor 0x8408 else crc shr 1 // 0x8408 = reverse 0x1021
+        }
+    }
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_CCITT_FALSE：多项式x16+x12+x5+1（0x1021），初始值0xFFFF，低位在后，高位在前，结果与0x0000异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16ccittFalse(): ByteArray {
+    var crc = 0xffff
+    for (j in this) {
+        crc = crc xor (j.toInt() shl 8)
+        for (i in 0..7) {
+            crc =
+                if (crc and 0x8000 != 0) crc shl 1 xor 0x1021 else crc shl 1 // 0x1021 = reverse 0x8408
+        }
+    }
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_XMODEM：多项式x16+x12+x5+1（0x1021），初始值0x0000，低位在后，高位在前，结果与0x0000异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16xmodem(): ByteArray {
+    var crc = 0x0000
+    for (j in this) {
+        crc = crc xor (j.toInt() shl 8)
+        for (i in 0..7) {
+            crc =
+                if (crc and 0x8000 != 0) crc shl 1 xor 0x1021 else crc shl 1 // 0x1021 = reverse 0x8408
+        }
+    }
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_X25：多项式x16+x12+x5+1（0x1021），初始值0xFFFF，低位在前，高位在后，结果与0xFFFF异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16x25(): ByteArray {
+    var crc = 0xffff
+    for (j in this) {
+        crc = crc xor j.toInt()
+        for (i in 0..7) {
+            crc = if (crc and 1 != 0) crc shr 1 xor 0x8408 else crc shr 1 // 0x8408 = reverse 0x1021
+        }
+    }
+    crc = crc.inv() // crc^0xffff
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
 
 /**
  * CRC16_MODBUS：多项式x16+x15+x2+1（0x8005），初始值0xFFFF，低位在前，高位在后，结果与0x0000异或
@@ -11,15 +78,111 @@ const val FF = 0xFF
  * @receiver ByteArray
  * @return ByteArray
  */
-fun ByteArray.crc16(): ByteArray {
-    var res = INITIAL_VALUE
-    for (data in this) {
-        res = res xor (data.toInt() and FF)
-        for (i in 0 until BITS_OF_BYTE) {
-            res = if (res and 0x0001 == 1) res shr 1 xor POLYNOMIAL else res shr 1
+fun ByteArray.crc16modbus(): ByteArray {
+    var crc = 0xffff
+    for (j in this) {
+        crc = crc xor (j.toInt() and 0xff) and 0xffff
+        for (i in 0..7) {
+            crc =
+                if (crc and 1 == 0) crc shr 1 else crc shr 1 xor 0xA001 and 0xffff // 0xA001 = reverse 0x8005
         }
     }
-    val lowByte: Byte = (res shr 8 and FF).toByte()
-    val highByte: Byte = (res and FF).toByte()
-    return byteArrayOf(lowByte, highByte)
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_IBM：多项式x16+x15+x2+1（0x8005），初始值0x0000，低位在前，高位在后，结果与0x0000异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16ibm(): ByteArray {
+    var crc = 0x0000
+    for (j in this) {
+        crc = crc xor j.toInt()
+        for (i in 0..7) {
+            crc =
+                if (crc and 1 == 0) crc shr 1 else crc shr 1 xor 0xA001  // 0xA001 = reverse 0x8005
+        }
+    }
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_MAXIM：多项式x16+x15+x2+1（0x8005），初始值0x0000，低位在前，高位在后，结果与0xFFFF异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16maxim(): ByteArray {
+    var crc = 0x0000
+    for (j in this) {
+        crc = crc xor j.toInt()
+        for (i in 0..7) {
+            crc =
+                if (crc and 1 == 0) crc shr 1 else crc shr 1 xor 0xA001  // 0xA001 = reverse 0x8005
+        }
+    }
+    crc = crc.inv() // crc^0xffff
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_USB：多项式x16+x15+x2+1（0x8005），初始值0xFFFF，低位在前，高位在后，结果与0xFFFF异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16usb(): ByteArray {
+    var crc = 0xffff
+    for (j in this) {
+        crc = crc xor j.toInt()
+        for (i in 0..7) {
+            crc =
+                if (crc and 1 == 0) crc shr 1 else crc shr 1 xor 0xA001  // 0xA001 = reverse 0x8005
+        }
+    }
+    crc = crc.inv() // crc^0xffff
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * CRC16_DNP：多项式x16+x13+x12+x11+x10+x8+x6+x5+x2+1（0x3D65），初始值0x0000，低位在前，高位在后，结果与0xFFFF异或
+ *
+ * @receiver ByteArray
+ * @return ByteArray
+ */
+fun ByteArray.crc16dnp(): ByteArray {
+    var crc = 0x0000
+    for (j in this) {
+        crc = crc xor j.toInt()
+        for (i in 0..7) {
+            crc =
+                if (crc and 1 != 0) crc shr 1 xor 0xA6BC else crc shr 1 // 0xA6BC = reverse 0x3D65
+        }
+    }
+    crc = crc.inv()
+    return byteArrayOf((crc shr 8 and 0xff).toByte(), (crc and 0xff).toByte())
+}
+
+/**
+ * crc16
+ *
+ * @receiver ByteArray
+ * @param method String
+ * @return ByteArray
+ */
+fun ByteArray.crc16(method: String = "MODBUS"): ByteArray {
+    return when (method) {
+        "CCITT" -> crc16ccitt()
+        "CCITT_FALSE" -> crc16ccittFalse()
+        "XMODEM" -> crc16xmodem()
+        "X25" -> crc16x25()
+        "MODBUS" -> crc16modbus()
+        "IBM" -> crc16ibm()
+        "MAXIM" -> crc16maxim()
+        "USB" -> crc16usb()
+        "DNP" -> crc16dnp()
+        else -> crc16modbus()
+    }
 }
--- Android/App_New/serialport/src/test/java/com/zktony/serialport/CRCTest.kt
+++ Android/App_New/serialport/src/test/java/com/zktony/serialport/CRCTest.kt
@@ -6,19 +6,14 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class CRCTest {
-
     @Test
-    fun crc1() {
+    fun crc16ccitt() {
         val bytes = ByteArray(256)
         for (i in 0..255) {
             bytes[i] = i.toByte()
         }
-        assertEquals("DE 6C", bytes.crc16().toHexString())
-    }
-
-    @Test
-    fun crc2() {
-        val bytes = byteArrayOf(
+        assertEquals("D8 41", bytes.crc16("CCITT").toHexString())
+        val bytes1 = byteArrayOf(
             0x01.toByte(),
             0x06.toByte(),
             0x0A.toByte(),
@@ -26,9 +21,160 @@ class CRCTest {
             0x00.toByte(),
             0x00.toByte()
         )
-        val crc16 = bytes.crc16()
-        assertEquals(0x12.toByte(), crc16[0])
-        assertEquals(0x8A.toByte(), crc16[1])
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("E3 1D", bytes1.crc16("CCITT").toHexString())
+    }
+
+    @Test
+    fun crc16ccittFalse() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("3F BD", bytes.crc16("CCITT_FALSE").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("EE 9E", bytes1.crc16("CCITT_FALSE").toHexString())
+    }
+
+    @Test
+    fun crc16xmodem() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("7E 55", bytes.crc16("XMODEM").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("E0 8E", bytes1.crc16("XMODEM").toHexString())
+    }
+
+    @Test
+    fun crc16x25() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("60 CF", bytes.crc16("X25").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("14 92", bytes1.crc16("X25").toHexString())
+    }
+
+    @Test
+    fun crc16modbus() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("DE 6C", bytes.crc16("MODBUS").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("12 8A", bytes1.crc16("MODBUS").toHexString())
+    }
+
+    @Test
+    fun crc16ibm() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("11 F9", bytes.crc16("IBM").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("09 8A", bytes1.crc16("IBM").toHexString())
+    }
+
+    @Test
+    fun crc16maxim() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("EE 06", bytes.crc16("MAXIM").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("F6 75", bytes1.crc16("MAXIM").toHexString())
+    }
+
+    @Test
+    fun crc16usb() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("8A B9", bytes.crc16("USB").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("ED 75", bytes1.crc16("USB").toHexString())
+    }
+
+    @Test
+    fun crc16dnp() {
+        val bytes = ByteArray(256)
+        for (i in 0..255) {
+            bytes[i] = i.toByte()
+        }
+        assertEquals("DD 27", bytes.crc16("DNP").toHexString())
+        val bytes1 = byteArrayOf(
+            0x01.toByte(),
+            0x06.toByte(),
+            0x0A.toByte(),
+            0x00.toByte(),
+            0x00.toByte(),
+            0x00.toByte()
+        )
+        assertEquals("01060A000000", bytes1.toHexString(false))
+        assertEquals("BD 8A", bytes1.crc16("DNP").toHexString())
     }
 
 }
