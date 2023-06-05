cfa7af51816cbef61b46c28bf75933330de772e4
fool-xu <935905712@qq.com>
2023-06-05 16:44:38 +0800

    commit
    修正步数问题

--- C/code/HARDWARE/PWM/pwm.h
+++ C/code/HARDWARE/PWM/pwm.h
@@ -4,10 +4,10 @@
 //////////////////////////////////////////////////////////////////////////////////	 
 					  
 void TIM_GPIO_Config(void);
-void TIM1_PWM_Init(uint32_t arr,uint32_t psc);
-void TIM2_PWM_Init(uint32_t arr,uint32_t psc);	
-void TIM3_PWM_Init(uint32_t arr,uint32_t psc);
-void TIM8_PWM_Init(uint32_t arr,uint32_t psc);	
+void TIM1_PWM_Init(void);
+void TIM2_PWM_Init(void);	
+void TIM3_PWM_Init(void);
+void TIM8_PWM_Init(void);	
 	
 void TIMxCHxOutControl(TIM_TypeDef* TIMx,uint8_t channelx,uint8_t operation);
 void TIMxCH1OutControl(TIM_TypeDef* TIMx,uint8_t operation);	
