b4d32f18e72b9645c100f7f19cfc9b55d4785ca7
Original Light <15252114322@163.com>
2024-06-25 23:22:13 +0800

    [燕子矶] 0.0.1 Beta 9

---
+++ Android/App_YanZiJi/App/src/main/java/com/zktony/android/ui/components/DateTimePicker.kt
@@ -0,0 +1,153 @@
+package com.zktony.android.ui.components
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.zktony.android.utils.extra.dateFormat
+import java.util.Calendar
+import java.util.Date
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateTimePicker(
+    modifier: Modifier = Modifier,
+    dateMillis: Long,
+    onDateChange: (Long) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    // 上面的代码是一个AlertDialog组件，用于选择日期和时间。
+    // 第一步先选择日期，然后选择时间。
+    // 最上方加一个步骤指示器，显示当前步骤。
+
+
+    var step by remember { mutableIntStateOf(0) }
+    var selectedMillis by remember { mutableLongStateOf(dateMillis) }
+    val hour = Calendar.getInstance().apply { timeInMillis = dateMillis }.get(Calendar.HOUR_OF_DAY)
+    val minute = Calendar.getInstance().apply { timeInMillis = dateMillis }.get(Calendar.MINUTE)
+    val timePickerState = rememberTimePickerState(initialHour = hour, initialMinute = minute, is24Hour = true)
+    val datePackerState = rememberDatePickerState(initialSelectedDateMillis = dateMillis)
+
+    Dialog(onDismissRequest = onDismissRequest) {
+        Column(
+            modifier = modifier
+                .background(
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = MaterialTheme.shapes.small
+                )
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            when (step) {
+                0 -> {
+                    DatePicker(state = datePackerState)
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        // 取消按钮
+                        // 下一步按钮
+                        Button(
+                            modifier = Modifier.padding(end = 16.dp),
+                            onClick = { onDismissRequest() }) {
+                            Text(text = "Cancel")
+                        }
+                        Button(onClick = {
+                            selectedMillis = datePackerState.selectedDateMillis ?: selectedMillis
+                            // 下一步
+                            step = 1
+                        }) {
+                            Text(text = "Next")
+                        }
+                    }
+                }
+
+                1 -> {
+                    // 选择时间
+                    // 选择时间的代码
+                    // 从dateMillis中提取小时和分钟
+                    // 时间选择器
+                    TimePicker(state = timePickerState)
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        // 取消按钮
+                        // 上一步按钮
+                        // 确认按钮
+                        Button(
+                            modifier = Modifier.padding(end = 16.dp),
+                            onClick = { onDismissRequest() }) {
+                            Text(text = "Cancel")
+                        }
+                        // 上一步按钮
+                        Button(
+                            modifier = Modifier.padding(end = 16.dp),
+                            onClick = { step = 0 }) {
+                            Text(text = "Previous")
+                        }
+                        Button(onClick = {
+                            // 更新时间
+                            selectedMillis = Calendar.getInstance().apply {
+                                timeInMillis = selectedMillis
+                                set(Calendar.HOUR_OF_DAY, timePickerState.hour)
+                                set(Calendar.MINUTE, timePickerState.minute)
+                            }.timeInMillis
+                            onDateChange(selectedMillis)
+                        }) {
+                            Text(text = "OK")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DateTimePickerPreview() {
+    var dateMillis by remember { mutableLongStateOf(System.currentTimeMillis()) }
+    var showDialog by remember { mutableStateOf(false) }
+    if (showDialog) {
+        DateTimePicker(
+            dateMillis = dateMillis,
+            onDateChange = {
+                dateMillis = it
+                showDialog = false
+                Log.d("DateTimePicker", "onDateMillisChange: ${Date(it).dateFormat("yyyy-MM-dd HH:mm:ss")}")
+            },
+            onDismissRequest = { showDialog = false }
+        )
+    }
+    Column {
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Show Dialog")
+        }
+
+    }
+}
